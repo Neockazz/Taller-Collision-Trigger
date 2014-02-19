570cd89eb39af97f6c6411da73a9766d5f8004fd
Neockazz <sb101010@ingenieria.sanmartin.edu.co>
2014-02-19 18:20:13 -0500

    subida del scene

---
+++ Collision/Assets/OnCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollider : MonoBehaviour {
+	public float haveforce=12f;
+	
+	void OnCollisionEnter(Collision collider) {
+		Debug.Log ("enter colision");
+	}
+	
+	void OnCollisionStay (Collision collider) {
+		Debug.Log ("Stay collision");
+		
+	}
+	void OnCollisionExit(Collision collider){
+		Debug.Log ("Exit collision");
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("Enter trigger");
+	}
+	
+	void OnTriggerStay (Collider collider) {
+		collider.rigidbody.AddForce(Vector3.up * haveforce,ForceMode.Acceleration);
+		Debug.Log ("Trigger Stay");
+		
+	}
+	void OnTriggerExit(Collider collider){
+		Debug.Log ("Exit trigger");
+	}
+}
